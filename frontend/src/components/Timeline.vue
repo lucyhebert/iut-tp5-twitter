50d51b05319f61f0e9ada1f49f0c5848fa01ce77
lucyhebert <lucyhebert@free.fr>
2017-06-21 11:32:32 +0200

    Implémente étape 4

--- frontend/src/components/Timeline.vue
+++ frontend/src/components/Timeline.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="timeline">
     <ul>
-      <li v-for="tweet in tweets">{{tweet}}</li>
+      <li v-for="tweet in tweets">{{tweet.auteur}} : {{tweet.contenu}}</li>
     </ul>
   </div>
 </template>
 
 <script>
+  var tweet1 = {auteur: 'Lulu', contenu: 'Pouet-pouet'}
+  var tweet2 = {auteur: 'Nico', contenu: 'Prout'}
+  var tweet3 = {auteur: 'Guigui', contenu: 'Prout-pouet'}
+
   export default {
     name: 'timeline',
     data () {
       return {
-        tweets: ['tweet 1', 'tweet 2', 'tweet 3']
+        tweets: [tweet1, tweet2, tweet3]
       }
     }
   }
